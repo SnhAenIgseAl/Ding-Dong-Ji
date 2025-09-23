c6ca9bfca2b33db221e946cfee6af5831c9fb877
SnhAneIgseAl <1789561096@qq.com>
2025-09-23 19:03:07 +0800

    update 邮箱配置信息

--- server/config/plugins.ts
+++ server/config/plugins.ts
@@ -6,13 +6,13 @@ export default ({ env }) => ({
                 host: env('SMTP_HOST', 'smtp.qq.com'),
                 port: env('SMTP_PORT', 465),
                 auth: {
-                    user: env('SMTP_USERNAME', '1789561096@qq.com'),
-                    pass: env('SMTP_PASSWORD', 'hsatqvoogtyvbdbj'),
+                    user: env('SMTP_USERNAME', ''),
+                    pass: env('SMTP_PASSWORD', ''),
                 },
             },
             settings: {
-                defaultFrom: '1789561096@qq.com',
-                defaultReplyTo: '1789561096@qq.com',
+                defaultFrom: '',
+                defaultReplyTo: '',
             }
         }
     }
